f9641522093a7e3b1d561e145b22a849ff925c44
Stanley Stuart <stanley@stan.li>
2016-02-25 11:52:44 -0600

    add a staging heroku app
    Master now gets auto-deployed by Travis to staging when stuff is merged
    to master. To deploy to production, push stuff to the `production` branch.

--- infrastructure/heroku.tf
+++ infrastructure/heroku.tf
@@ -1,5 +1,6 @@
 # Expected to be defined as an environment variable
 variable heroku_production_app_name {}
+variable heroku_staging_app_name {}
 
 resource "heroku_app" "production" {
   name = "${var.heroku_production_app_name}"
@@ -15,3 +16,16 @@ resource "heroku_app" "production" {
   }
 }
 
+resource "heroku_app" "staging" {
+  name = "${var.heroku_staging_app_name}"
+  region = "us"
+
+  config_vars {
+    AWS_ACCESS_KEY_ID = "${aws_iam_access_key.deploy.id}"
+    AWS_SECRET_ACCESS_KEY = "${aws_iam_access_key.deploy.secret}"
+    AWS_BUCKET_NAME = "${aws_s3_bucket.production.bucket}"
+    AWS_BUCKET_REGION = "${var.aws_region}"
+
+    HEROKU_EMBER_CLI_DEPLOY = "true"
+  }
+}
