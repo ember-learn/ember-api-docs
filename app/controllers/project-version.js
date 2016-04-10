6cf366826e0cb982cac6e30cbcd264c0829b0add
Gaurav Munjal <gmunjal@hedgeserv.com>
2016-04-10 09:07:23 -0400

    Show only minor versions in dropdown

--- app/controllers/project-version.js
+++ app/controllers/project-version.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
+import _ from 'lodash/lodash';
 import semverCompare from 'npm:semver-compare';
+import getMinorVersion from "../utils/get-minor-version";
 
 export default Ember.Controller.extend({
   classesIDs: Ember.computed('model', function() {
@@ -17,10 +19,21 @@ export default Ember.Controller.extend({
 
   projectVersions: Ember.computed('model', function() {
     const projectVersions = this.get('model.project.projectVersions');
-    return projectVersions.toArray().sort(function(a, b) {
+    let versions = projectVersions.toArray().sort(function(a, b) {
       const a_ver = a.get('id').split("-")[1];
       const b_ver = b.get('id').split("-")[1];
       return semverCompare(b_ver, a_ver);
+    });
+    versions.forEach(function(version) {
+      const versionString = version.get('id').split("-")[1];
+      const minorVersion = getMinorVersion(versionString);
+      version.set('minorVersion', minorVersion);
+    });
+    let groupedVersions = _.groupBy(versions, function(version) {
+      return version.get('minorVersion');
+    });
+    return _.values(groupedVersions).map(function(groupedVersion) {
+      return groupedVersion[0];
     });
   }),
 
