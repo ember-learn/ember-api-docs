66d77d07ef7b6818ae0a8cb64d7a5c81123d5086
Gaurav Munjal <gmunjal@hedgeserv.com>
2016-06-10 10:54:53 -0400

    Fix tests on travis
    Try to fix travis again
    
    Try to fix travis again

--- config/environment.js
+++ config/environment.js
@@ -11,7 +11,7 @@ module.exports = function(environment) {
     EmberENV: {
       EXTEND_PROTOTYPES: false,
       FEATURES: {
-        'ds-references': true,
+        //'ds-references': true,
         //'ember-glimmer': true
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
