99b8fd98bd7d86d67ea54e61c860237b09f055f4
Gaurav Munjal <gmunjal@linkedin.com>
2016-11-11 15:34:40 -0800

    Hide private modules and namespaces

--- app/models/project-version.js
+++ app/models/project-version.js
@@ -8,5 +8,9 @@ export default DS.Model.extend({
   namespaces: DS.hasMany('namespace', {async: true}),
   'public-classes': DS.hasMany('class', {async: true}),
   'private-classes': DS.hasMany('class', {async: true}),
+  'public-modules': DS.hasMany('module', {async: true}),
+  'private-modules': DS.hasMany('module', {async: true}),
+  'public-namespaces': DS.hasMany('namespace', {async: true}),
+  'private-namespaces': DS.hasMany('namespace', {async: true}),
   project: DS.belongsTo('project')
 });
