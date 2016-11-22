c8323477920c6c395ac44a230d8d7b9a0f5ed4cd
Gaurav Munjal <gmunjal@linkedin.com>
2016-11-22 09:47:21 -0800

    Add tab navigation acceptance tests

---
+++ tests/acceptance/tab-nav-test.js
@@ -0,0 +1,37 @@
+import { test } from 'qunit';
+
+import moduleForAcceptance from 'ember-api-docs/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | tab navigation');
+
+test('switching tabs', function(assert) {
+  visit('/ember/1.0.0/classes/Ember.Component');
+  click('.access-checkbox:contains(Inherited)');
+  click('.tabbed-layout__menu li:contains(Methods)');
+
+  andThen(() => {
+    assert.equal(currentURL(), '/ember/1.0.0/classes/Ember.Component/methods?anchor=', 'navigated to methods');
+    assert.ok(find('.tabbed-layout__menu li:contains(Methods)').hasClass('tabbed-layout__menu__item_selected'), 'methods tab selected');
+  });
+
+  click('.tabbed-layout__menu li:contains(Properties)');
+
+  andThen(() => {
+    assert.equal(currentURL(), '/ember/1.0.0/classes/Ember.Component/properties?anchor=', 'navigated to properties');
+    assert.ok(find('.tabbed-layout__menu li:contains(Properties)').hasClass('tabbed-layout__menu__item_selected'), 'properties tab selected');
+  });
+
+  click('.tabbed-layout__menu li:contains(Events)');
+
+  andThen(() => {
+    assert.equal(currentURL(), '/ember/1.0.0/classes/Ember.Component/events?anchor=', 'navigated to events');
+    assert.ok(find('.tabbed-layout__menu li:contains(Events)').hasClass('tabbed-layout__menu__item_selected'), 'events tab selected');
+  });
+
+  click('.tabbed-layout__menu li:contains(Index)');
+
+  andThen(() => {
+    assert.equal(currentURL(), '/ember/1.0.0/classes/Ember.Component', 'navigated to index');
+    assert.ok(find('.tabbed-layout__menu li:contains(Index)').hasClass('tabbed-layout__menu__item_selected'), 'index tab selected');
+  });
+});
