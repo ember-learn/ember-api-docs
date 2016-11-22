51cea2ebdb5962149866c6cc4fca1c3a45b15557
Gaurav Munjal <gmunjal@linkedin.com>
2016-11-22 15:37:43 -0800

    Use remarkable to render markdown and highlightjs to render code snippets

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -23,22 +23,12 @@ module.exports = function(defaults) {
     }
   });
 
+  app.import(app.bowerDirectory + '/highlightjs/styles/github.css');
+  app.import(app.bowerDirectory + '/highlightjs-line-numbers.js/src/highlightjs-line-numbers.js');
+
   if (!process.env.EMBER_CLI_FASTBOOT && process.env.EMBER_ENV === 'test') {
     app.import(app.bowerDirectory + '/pouchdb/dist/pouchdb.memory.js');
   }
-
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
 
   return app.toTree();
 };
