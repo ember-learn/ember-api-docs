ad2a02a96e8bed53593e8c59bbc467a16a0df04f
Todd Jordan <todd.jordan@inin.com>
2016-11-21 22:56:53 -0500

    Show filter across tabs, persist filter in ui

--- app/controllers/project-version/class.js
+++ app/controllers/project-version/class.js
@@ -5,6 +5,8 @@ import ParentNameMixin from 'ember-api-docs/mixins/parent-name';
 const { computed, Controller } = Ember;
 
 export default Controller.extend(ParentNameMixin, {
+  filterData: Ember.inject.service(),
+
   githubLinkTarget: computed('model.{file,line}', function() {
     const file = this.get('model.file');
     const line = this.get('model.line');
@@ -12,5 +14,11 @@ export default Controller.extend(ParentNameMixin, {
     const version = this.get('model.projectVersion.version');
     const githubDir = githubMap[project];
     return `https://github.com/${githubDir}/tree/v${version}/${file}#L${line}`;
-  })
+  }),
+
+  actions: {
+    updateFilter(filter) {
+      this.toggleProperty(`filterData.${filter}`);
+    }
+  }
 });
