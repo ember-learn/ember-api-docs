4b19a516b327388c17ccafe814a6e4db9dbc0149
Todd Jordan <todd.jordan@inin.com>
2016-11-30 16:58:00 -0500

    Adding pencil link on classes and modules

--- app/controllers/project-version/class.js
+++ app/controllers/project-version/class.js
@@ -1,20 +1,10 @@
 import Ember from 'ember';
-import githubMap from 'ember-api-docs/utils/github-map';
 import ParentNameMixin from 'ember-api-docs/mixins/parent-name';
 
-const { computed, Controller } = Ember;
+const { Controller } = Ember;
 
 export default Controller.extend(ParentNameMixin, {
   filterData: Ember.inject.service(),
-
-  githubLinkTarget: computed('model.{file,line}', function() {
-    const file = this.get('model.file');
-    const line = this.get('model.line');
-    const project = this.get('model.project.id');
-    const version = this.get('model.projectVersion.version');
-    const githubDir = githubMap[project];
-    return `https://github.com/${githubDir}/tree/v${version}/${file}#L${line}`;
-  }),
 
   actions: {
     updateFilter(filter) {
