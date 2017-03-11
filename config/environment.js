19b8b239abbc74d0c7b2236966317b1f42eb1f24
Michael Schinis <mschinis@gmail.com>
2017-03-11 23:13:39 +0000

    fix(environment): Add algolia to content security policy

--- config/environment.js
+++ config/environment.js
@@ -36,7 +36,7 @@ module.exports = function(environment) {
       enabled: false
     };
     ENV.contentSecurityPolicy = {
-      "connect-src": "'self' http://localhost:5984 https://*.cloudant.com",
+      "connect-src": "'self' http://localhost:5984 https://*.cloudant.com *.algolia.net",
       "script-src": "'self' unsafe-inline use.typekit.net",
       "font-src": "'self' data://* use.typekit.net",
       "img-src": "'self' p.typekit.net",
