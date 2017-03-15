04d9d765279fcf1cae7f25dd312bfbdfcaed239b
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2017-03-15 09:45:48 +0530

    add template linter

---
+++ .template-lintrc.js
@@ -0,0 +1,10 @@
+/* eslint-env node:true */
+'use strict';
+
+module.exports = {
+  extends: 'recommended',
+  rules: {
+    'bare-strings': false,
+    'block-indentation': false //TODO siva fix this
+  }
+};
