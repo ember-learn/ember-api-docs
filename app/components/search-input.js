44d42a76b5e5e87fd62ee31ba4f454db2d16e526
Michael Schinis <mschinis@gmail.com>
2017-03-21 09:53:51 +0000

    feat(search-input): Improve search and display results

--- app/components/search-input.js
+++ app/components/search-input.js
@@ -15,7 +15,8 @@ export default Ember.Component.extend({
   _results: Ember.A(),
   _groupedResults: Ember.computed('_results.[]', function () {
     return get(this, '_results').reduce((previous, current) => {
-      const lvl0 = get(current, 'hierarchy.lvl0').toLowerCase();
+      // Remap all lowercase usages of 'guides' to 'Guides'
+      let lvl0 = Ember.String.capitalize(get(current, 'hierarchy.lvl0'));
       // If lvl0 doesn't exist in the resulting object, create the array
       if (!previous[lvl0]) {
         previous[lvl0] = Ember.A();
@@ -25,16 +26,25 @@ export default Ember.Component.extend({
       return previous;
     }, {});
   }),
-
+  _resultTetherConstraints: [
+    {
+      to: 'window',
+      attachement: 'together'
+    }
+  ],
   actions: {
     search(query) {
       const client = get(this, '_searchClient');
       const projectVersion = get(this, '_projectVersion');
+
       const queries = [
         {
           indexName: 'emberjs_versions',
           query,
-          facetFilters: [[`version:${projectVersion}`,'tags:api']]
+          params: {
+            restrictSearchableAttributes: ['hierarchy.lvl0', 'hierarchy.lvl1','hierarchy.lvl2'],
+            facetFilters: [[`version:${projectVersion}`,'tags:api']]
+          }
         }
       ];
 
@@ -42,13 +52,18 @@ export default Ember.Component.extend({
         .search(queries)
         .then(res => {
           const results = get(res, 'results.0.hits');
+
+          results.forEach(result => console.log(result))
+
           return get(this, '_results')
             .clear()
             .addObjects(results);
         });
     },
     clearResults() {
-      get(this, '_results').clear();
+      // Ember.run.later(this, function () {
+      //   get(this, '_results').clear();
+      // }, 200);
     }
   }
 });
