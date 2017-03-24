5981252e5200b57a2b6263b06fb3411d3fb7877f
David Baker <dbaker@acorncomputersolutions.com>
2017-03-24 11:53:10 -0600

    Removing Fastboot additional url that seems to break things when proxied

--- app/router.js
+++ app/router.js
@@ -3,7 +3,7 @@ import config from './config/environment';
 
 const Router = Ember.Router.extend({
   location: config.locationType,
-  rootURL: config.routerRootURL
+  rootURL: config.rootURL
 });
 
 Router.map(function() {
