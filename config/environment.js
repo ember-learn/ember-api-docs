472ac0cd4a52224868af6269b790840dd9b34b50
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2017-03-27 12:40:51 +0530

    Use teh secure fastly endpoint

--- config/environment.js
+++ config/environment.js
@@ -7,7 +7,7 @@ module.exports = function(environment) {
     rootURL: '/',
     routerRootURL: '/',
     locationType: 'auto',
-    API_HOST: 'http://ember-api-cdn.perf.site.global.prod.fastly.net',
+    API_HOST: 'https://ead-sk.global.ssl.fastly.net',
     IS_FASTBOOT: !!process.env.EMBER_CLI_FASTBOOT,
     EmberENV: {
       EXTEND_PROTOTYPES: false,
