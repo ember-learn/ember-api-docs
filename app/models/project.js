ee664768789f725442998005d9827adef53b2ce8
martinmalinda <malindacz@gmail.com>
2017-03-27 15:57:44 +0200

    move sorted project versions to project model

--- app/models/project.js
+++ app/models/project.js
@@ -1,9 +1,31 @@
 import DS from 'ember-data';
+import Ember from 'ember';
+import semverCompare from 'npm:semver-compare';
+import getMinorVersion from "../utils/get-minor-version";
+import _ from 'lodash';
+
 
 const {Model, attr, hasMany} = DS;
+const {computed} = Ember;
 
 export default Model.extend({
   name: attr(),
   githubUrl: attr(),
-  projectVersions: hasMany('project-version', {async: true})
+  projectVersions: hasMany('project-version', {async: true}),
+
+  latestProjectVersion: computed.alias('sortedProjectVersions.firstObject'),
+  sortedProjectVersions: computed(function() {
+    let projectVersions =  this.hasMany('projectVersions').ids().map(id => id.split('-')[1]);
+    let sortedVersions = projectVersions.sort((a, b) => semverCompare(b, a));
+
+    sortedVersions = sortedVersions.map((version) => {
+      const minorVersion = getMinorVersion(version);
+      return { id: version, minorVersion };
+    });
+    
+    let groupedVersions = _.groupBy(sortedVersions, version => version.minorVersion);
+
+    return _.values(groupedVersions).map(groupedVersion => groupedVersion[0]);
+  }),
+
 });
