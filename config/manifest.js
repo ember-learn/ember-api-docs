4c6a4e9d51a479ace4efae9afd013abc53aeceef
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2017-03-28 00:49:16 +0530

    Launcher manifest changes

--- config/manifest.js
+++ config/manifest.js
@@ -16,8 +16,28 @@ module.exports = function(environment /*, appConfig */) {
     scope: environment === 'production' ? '/api-new/' : '/',
     icons: [
       {
-        src: '/assets/images/ember-logo.png',
-        sizes: '1200x1016',
+        src: '/assets/images/launcher-icon-4x.png',
+        sizes: '192x163',
+        type: 'image/png'
+      }, {
+        src: '/assets/images/launcher-icon-3x.png',
+        sizes: '144x122',
+        type: 'image/png'
+      }, {
+        src: '/assets/images/launcher-icon-2x.png',
+        sizes: '96x81',
+        type: 'image/png'
+      }, {
+        src: '/assets/images/launcher-icon-1-5x.png',
+        sizes: '72x61',
+        type: 'image/png'
+      }, {
+        src: '/assets/images/launcher-icon-1x.png',
+        sizes: '48x41',
+        type: 'image/png'
+      } , {
+        src: '/assets/images/launcher-icon-0-75x.png',
+        sizes: '36x30',
         type: 'image/png'
       }
     ],
