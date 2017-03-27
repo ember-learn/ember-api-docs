8da67e5a66b4142656d52b33eb7340359d79235c
martinmalinda <malindacz@gmail.com>
2017-03-27 09:01:50 +0200

    fix typo and do not export requestIdleCb function

--- app/utils/request-idle-callback.js
+++ app/utils/request-idle-callback.js
@@ -2,11 +2,11 @@ import Ember from 'ember';
 
 const { RSVP, run } = Ember;
 
-export function requestIdleCb(cb, opts) {
+function requestIdleCb(cb, opts) {
   if ('requestIdleCallback' in window) {
     return requestIdleCallback(cb, opts);
   } else {
-    return run.shceduleOnce('afterRender', cb);
+    return run.scheduleOnce('afterRender', cb);
   }
 }
 
