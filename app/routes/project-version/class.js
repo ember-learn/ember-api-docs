6b56454c2e71a847c153fe5e81673a2c46060394
Todd Jordan <todd.jordan@inin.com>
2017-05-24 01:45:28 -0400

    Revert "Handle /release requests (#209)"
    This reverts commit ffd2eb5bbfb604ff337cb290bce49203fb40eeb6.

--- app/routes/project-version/class.js
+++ app/routes/project-version/class.js
@@ -17,9 +17,8 @@ export default Ember.Route.extend({
   },
 
   getModel(typeName, params, transition) {
-    const projectModel = this.modelFor('project-version').get('project.content');
     const projectID = transition.params['project-version'].project;
-    const version = projectModel.getProjectVersion(transition.params['project-version'].project_version);
+    const version = transition.params['project-version'].project_version;
     const klass = params[typeName];
     return this.find(typeName, `${projectID}-${version}-${klass}`);
   },
