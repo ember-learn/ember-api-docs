40d1e33540eeb47c331e2558062e01d9c8cff823
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-06-09 17:58:30 +0200

    Update "ember-cli-qunit" to v4.0.0

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,8 @@
 import resolver from './helpers/resolver';
 import { setResolver } from 'ember-qunit';
 import loadEmberExam from 'ember-exam/test-support/load';
+import { start } from 'ember-cli-qunit';
 
 setResolver(resolver);
 loadEmberExam();
+start();
