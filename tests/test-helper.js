27907c8b8b87295b1f6453c4b3bc91255fc1b644
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-06-09 17:57:10 +0200

    Update "ember-exam" to v0.7.0

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,6 @@
 import resolver from './helpers/resolver';
-import {
-  setResolver
-} from 'ember-qunit';
+import { setResolver } from 'ember-qunit';
+import loadEmberExam from 'ember-exam/test-support/load';
 
 setResolver(resolver);
+loadEmberExam();
