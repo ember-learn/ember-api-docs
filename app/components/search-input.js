62bb97574445fe14271e26953778ed6d77347f92
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2017-06-12 08:58:20 +0530

    Make algolia search fastboot compatible
    - Switch to use ember-algolia
    - Added a debounce on the search input to save some algolia calls
    - Added ember-concurrency for search debounce
    - Style the native search cancel button on chrome to be positioned
    little further from the edge

--- app/components/search-input.js
+++ app/components/search-input.js
@@ -1,16 +1,18 @@
 import Ember from 'ember';
-import algoliasearch from 'npm:algoliasearch';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
-import config from 'ember-api-docs/config/environment';
+import {task, timeout} from 'ember-concurrency';
+
+const SEARCH_DEBOUNCE_PERIOD = 300;
 
 export default Ember.Component.extend({
   // Public API
   value: '',
 
+  _searchClient: Ember.inject.service('algolia'),
+
   // Private API
   classNames: ['search-input'],
-  _searchClient: algoliasearch(config.ALGOLIA_APP_ID, config.ALGOLIA_API_KEY),
   _projectService: Ember.inject.service('project'),
   _projectVersion: Ember.computed.alias('_projectService.standardisedVersion'),
   _results: Ember.A(),
@@ -21,42 +23,56 @@ export default Ember.Component.extend({
       pin: ['left','right']
     }
   ],
-  actions: {
-    async search(query) {
-      const client = get(this, '_searchClient');
-      const projectVersion = get(this, '_projectVersion');
 
-      // Hide and don't run query if there's no search query
-      // if (!query) {
-      //   return set(this, '_focused', false);
-      // }
+  search: task(function * (query) {
 
-      const queries = [
-        {
-          indexName: 'emberjs_versions',
-          query,
-          params: {
-            hitsPerPage: 15,
-            restrictSearchableAttributes: ['hierarchy.lvl0', 'hierarchy.lvl1', 'hierarchy.lvl2', 'hierarchy.lvl3', 'hierarchy.lvl4'],
-            facetFilters: [[`version:${projectVersion}`,'tags:api']]
-          }
+    yield timeout(SEARCH_DEBOUNCE_PERIOD);
+
+    const client = get(this, '_searchClient');
+    const projectVersion = get(this, '_projectVersion');
+
+    // Hide and don't run query if there's no search query
+    // if (!query) {
+    //   return set(this, '_focused', false);
+    // }
+
+    const params = {
+      hitsPerPage: 15,
+      restrictSearchableAttributes: ['hierarchy.lvl0', 'hierarchy.lvl1', 'hierarchy.lvl2', 'hierarchy.lvl3', 'hierarchy.lvl4'],
+      facetFilters: [[`version:${projectVersion}`,'tags:api']]
+    };
+
+    const searchObj = {
+      indexName: 'emberjs_versions',
+      query
+    };
+
+    let res = yield new Ember.RSVP.Promise((resolve, reject) => {
+      client.search(searchObj, params, function(err, content) {
+        if (err) {
+          reject(err);
         }
-      ];
+        resolve(content);
+      })
+    });
 
-      let res = await client.search(queries);
+    const results = get(res, 'hits');
+    return get(this, '_results')
+      .clear()
+      .addObjects(results);
+  }).restartable(),
 
-      const results = get(res, 'results.0.hits');
-      return get(this, '_results')
-        .clear()
-        .addObjects(results);
-    },
+  actions: {
+
     onfocus() {
       set(this, '_focused', true);
     },
+
     onblur() {
       Ember.run.later(this, function () {
         set(this, '_focused', false);
       }, 200);
     }
+
   }
 });
