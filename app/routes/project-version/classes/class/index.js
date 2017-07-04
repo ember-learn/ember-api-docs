b03716c91aaf290245d27ab6eceb1795a3f2f50e
Todd Jordan <todd.jordan@inin.com>
2017-07-04 00:47:04 -0400

    Support fastboot plus hash params

--- app/routes/project-version/classes/class/index.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-import HashRedirectMixin from '../../../../mixins/hash-redirect';
-
-export default Ember.Route.extend(HashRedirectMixin, {
-  templateName: 'class-index'
-});
--- app/routes/project-version/namespaces/namespace/index.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-import HashRedirectMixin from 'ember-api-docs/mixins/hash-redirect';
-
-export default Ember.Route.extend(HashRedirectMixin, {
-  templateName: 'class-index'
-
-});
