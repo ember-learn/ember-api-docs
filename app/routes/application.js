312071cc18cdd8944a05ff84f1f64d63d8da1a86
martinmalinda <malindacz@gmail.com>
2017-07-06 07:17:08 +0200

    rely on ember-cli-head to set document title

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,7 +5,7 @@ const { set, inject } = Ember;
 
 export default Ember.Route.extend({
   headData: inject.service(),
-  title: function(tokens) {
+  title(tokens) {
     const reversed = Ember.makeArray(tokens).reverse();
     const title = `${reversed.join(' - ')} - Ember API Documentation`;
     set(this, 'headData.title', title);
