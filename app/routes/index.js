23f03e4ae0e9c10e0c6e16655eedcb601a9bce76
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2017-07-08 23:48:48 +0530

    Customize fastboot to handle redirects

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,24 +1,8 @@
 import Ember from 'ember';
-import ENV from 'ember-api-docs/config/environment';
-
-const { inject: { service } } = Ember;
 
 export default Ember.Route.extend({
 
-  fastboot: service(),
-
   beforeModel() {
-    let fastboot = this.get('fastboot');
-
-    // This app doesn't get its own custom domain since its part of emberjs.com site
-    // under the `/api/` path. Since there's no custom domain or ssl keys to
-    // setup, we're convincing the fastboot server into believing we're serving over ssl.
-    if (fastboot.get('isFastBoot') && ENV.environment === 'production') {
-      let request = fastboot.get('request');
-      request.protocol = 'https';
-      fastboot.set('request', request);
-    }
-
     return this.transitionTo('project', 'ember');
   }
 
