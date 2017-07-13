83a58df0f12608f9be2d1565e9d72a862b4fcaf0
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2017-07-13 19:32:36 +0530

    Bringing back web app manifest

--- config/manifest.js
+++ config/manifest.js
@@ -1,45 +1,44 @@
-/*eslint-env node:true*/
+/* eslint-env node */
 'use strict';
 
 module.exports = function(environment /*, appConfig */) {
   // See https://github.com/san650/ember-web-app#documentation for a list of
   // supported properties
-  var docsSlug = process.env.DOCS_SLUG ? process.env.DOCS_SLUG : '/api-new/';
 
   return {
     name: 'Ember API Docs',
     short_name: 'Ember API',
     description: 'Ember & Ember Data API Documentation',
-    start_url: environment === 'production' ? docsSlug : '/',
+    start_url: environment.routerRootURL,
     display: 'standalone',
     background_color: '#FDFDFD',
     theme_color: '#f67862',
-    // prepend: environment === 'production' && 'FASTLY_CDN_URL' in process.env ? FASTLY_CDN_URL.env : '',
-    scope: environment === 'production' ? docsSlug : '/',
+    scope: environment.routerRootURL,
+    lang: 'en-US',
     icons: [
       {
-        src: '/assets/images/launcher-icon-4x.png',
-        sizes: '192x163',
+        src: '/assets/images/launch-icon-4x.png',
+        sizes: '192x192',
         type: 'image/png'
       }, {
-        src: '/assets/images/launcher-icon-3x.png',
-        sizes: '144x122',
+        src: '/assets/images/launch-icon-3x.png',
+        sizes: '144x144',
         type: 'image/png'
       }, {
-        src: '/assets/images/launcher-icon-2x.png',
-        sizes: '96x81',
+        src: '/assets/images/launch-icon-2x.png',
+        sizes: '96x96',
         type: 'image/png'
       }, {
-        src: '/assets/images/launcher-icon-1-5x.png',
-        sizes: '72x61',
+        src: '/assets/images/launch-icon-1-5x.png',
+        sizes: '72x72',
         type: 'image/png'
       }, {
-        src: '/assets/images/launcher-icon-1x.png',
-        sizes: '48x41',
+        src: '/assets/images/launch-icon-1x.png',
+        sizes: '48x48',
         type: 'image/png'
       } , {
-        src: '/assets/images/launcher-icon-0-75x.png',
-        sizes: '36x30',
+        src: '/assets/images/launch-icon-0-75x.png',
+        sizes: '36x36',
         type: 'image/png'
       }
     ],
