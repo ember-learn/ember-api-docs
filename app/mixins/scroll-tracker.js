24391cf87ab1111f24c90ac3f836b909ddda6b6d
akash.dsouza <akash.dsouza@zohocorp.com>
2017-08-07 14:57:21 +0530

    [Bugfix]Fix autoscroll to methods issue

--- app/mixins/scroll-tracker.js
+++ app/mixins/scroll-tracker.js
@@ -1,5 +1,7 @@
 import Mixin from '@ember/object/mixin';
 import { inject as service } from '@ember/service';
+import $ from 'jquery';
+import config from 'ember-api-docs/config/environment';
 
 export default Mixin.create({
 
@@ -11,7 +13,18 @@ export default Mixin.create({
     },
 
     didTransition() {
-      this.get('scrollPositionReset').doReset();
+      this._super();
+      let isAnchorEmpty = window && window.location && window.location.search === '?anchor='
+      if ((typeof FastBoot === 'undefined') && isAnchorEmpty ) {
+        let elem = $('#methods');
+        let offset = (elem && elem.offset && elem.offset()) ? elem.offset().top : null;
+        if (offset) {
+          const navMenuHeight = $('header').outerHeight();
+          $(config.APP.scrollContainerSelector).scrollTop(offset - navMenuHeight - 10);
+        }
+      } else {
+        this.get('scrollPositionReset').doReset();
+      }
     }
   }
 });
