55b9b0bc888e4bd14b81630e59cc2e13eb9a4fca
Todd Jordan <todd.jordan@gmail.com>
2017-10-25 01:33:16 -0400

    version switching fixes (#390)
    * use project service instead of modelFor (which sometimes fails)
    
    * handle switch for additional scenarios
    
    * Add version switching tests, and fix issues found while testing

--- app/routes/project-version.js
+++ app/routes/project-version.js
@@ -1,5 +1,6 @@
 import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
+import semverCompare from 'npm:semver-compare';
 
 export default Route.extend({
 
@@ -45,40 +46,70 @@ export default Route.extend({
 
   actions: {
     updateProject(project, ver /*, component */) {
-      const projectVersionID = ver.compactVersion;
+      let projectVersionID = ver.compactVersion;
       let endingRoute, routeName;
       switch (routeName = this.router.currentRouteName) {
         case 'project-version.classes.class': {
-          const className = this.modelFor(routeName).get('name');
+          let className = this.modelFor(routeName).get('name');
           endingRoute = `classes/${className}`;
           break;
         }
         case 'project-version.classes.class.index': {
-          const className = this.modelFor('project-version.classes.class').get('name');
+          let className = this.modelFor('project-version.classes.class').get('name');
           endingRoute = `classes/${className}`;
           break;
         }
         case 'project-version.modules.module.index': {
-          const moduleName = this.paramsFor('project-version.modules.module').module;
+          let moduleName = this.paramsFor('project-version.modules.module').module;
           endingRoute = `modules/${moduleName}`;
           break;
         }
         case 'project-version.namespaces.namespace.index': {
-          const namespaceName = this.paramsFor('project-version.namespaces.namespace').namespace;
+          let namespaceName = this.paramsFor('project-version.namespaces.namespace').namespace;
           endingRoute = `namespaces/${namespaceName}`;
           break;
         }
+        case 'project-version.classes.class.methods.index': {
+          let className = this.modelFor('project-version.classes.class').get('name');
+          endingRoute = `classes/${className}/methods`;
+          break;
+        }
+        case 'project-version.classes.class.events.index': {
+          let className = this.modelFor('project-version.classes.class').get('name');
+          endingRoute = `classes/${className}/events`;
+          break;
+        }
+        case 'project-version.classes.class.properties.index': {
+          let className = this.modelFor('project-version.classes.class').get('name');
+          endingRoute = `classes/${className}/properties`;
+          break;
+        }
+        case 'project-version.classes.class.methods.method': {
+          let className = this.modelFor('project-version.classes.class').get('name');
+          let methodName = this.paramsFor('project-version.classes.class.methods.method').method;
+          endingRoute = `classes/${className}/methods/${methodName}?anchor=${methodName}`;
+          break;
+        }
+        case 'project-version.classes.class.events.event': {
+          let className = this.modelFor('project-version.classes.class').get('name');
+          let eventName = this.paramsFor('project-version.classes.class.events.event').event;
+          endingRoute = `classes/${className}/events/${eventName}?anchor=${eventName}`;
+          break;
+        }
+        case 'project-version.classes.class.properties.property': {
+          let className = this.modelFor('project-version.classes.class').get('name');
+          let propertyName = this.paramsFor('project-version.classes.class.properties.property').property;
+          endingRoute = `classes/${className}/properties/${propertyName}?anchor=${propertyName}`;
+          break;
+        }
         default:
+          endingRoute = '';
           break;
       }
       // if the user is navigating to/from api versions >= 2.16, take them
       // to the home page instead of trying to translate the url
-      let shouldConvertPackages = this.shouldConvertPackages(ver, this.modelFor(routeName).get('id'))
-      if (shouldConvertPackages) {
-        endingRoute = null;
-      }
-
-      if (endingRoute) {
+      let shouldConvertPackages = this.shouldConvertPackages(ver, this.get('projectService.version'));
+      if (!shouldConvertPackages) {
         this.transitionTo(`/${project}/${projectVersionID}/${endingRoute}`);
       } else {
         this.transitionTo(`/${project}/${projectVersionID}`);
@@ -89,10 +120,10 @@ export default Route.extend({
   // whether the user is switching versions for a 2.16 docs release or later.
   // The urls for pre-2.16 classes and later packages are quite different
   shouldConvertPackages(targetVer, previousVer) {
-    let targetVerNumbers = targetVer.id.split('.')
-    let targetVersion = Number(targetVerNumbers[0] + '.' + targetVerNumbers[1])
-    let prevVerNumbers = previousVer.split('-')[1].split('.')
-    let previousVersion = Number(prevVerNumbers[0] + '.' + prevVerNumbers[1])
-    return (previousVersion >= 2.16 || targetVersion >= 2.16)
+    let targetVersion = targetVer.id.split('.').slice(0, 2).join('.');
+    let previousVersion = previousVer.split('.').slice(0,2).join('.');
+    let previousComparison = semverCompare(previousVersion, '2.16');
+    let targetComparison = semverCompare(targetVersion, '2.16');
+    return (previousComparison < 0 && targetComparison >=0) || (previousComparison >=0 && targetComparison < 0);
   }
 });
