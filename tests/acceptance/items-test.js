c3d5ad9d93cc7c79d78f4279aa9c223fe5239d17
Alex Kanunnikov <Aleksandr_Kanunnikov@epam.com>
2017-11-01 15:02:26 +0300

    removed deprecation warnings

--- tests/acceptance/items-test.js
+++ tests/acceptance/items-test.js
@@ -1,13 +1,12 @@
 import moduleForAcceptance from 'ember-api-docs/tests/helpers/module-for-acceptance';
 import { test } from 'qunit';
 import { visit, click, findAll} from 'ember-native-dom-helpers';
-import testSelector from 'ember-test-selectors';
 
 moduleForAcceptance('Acceptance | ItemRoutes');
 
 test('Can navigate to method from class', async function(assert) {
   await visit('/ember/1.0/classes/Container');
-  await click(`.spec-method-list ${testSelector('item', 'child')} a`);
+  await click(`.spec-method-list ${'[data-test-item="child"]'} a`);
 
   assert.equal(currentURL(), '/ember/1.0/classes/Container/methods/child?anchor=child', 'navigated to method');
 });
@@ -22,28 +21,28 @@ test('Can navigate to method from method name', async function(assert) {
 
 test('Can navigate to property from class', async function(assert) {
   await visit('/ember/1.0/classes/Container');
-  await click(`.spec-property-list ${testSelector('item', 'cache')} a`);
+  await click(`.spec-property-list ${'[data-test-item="cache"]'} a`);
 
   assert.equal(currentURL(), '/ember/1.0/classes/Container/properties/cache?anchor=cache', 'navigated to property');
 });
 
 test('Can navigate to method from namespace', async function(assert) {
   await visit('/ember/1.0/namespaces/Ember');
-  await click(`.spec-method-list ${testSelector('item', 'A')} a`);
+  await click(`.spec-method-list ${'[data-test-item="A"]'} a`);
 
   assert.equal(currentURL(), '/ember/1.0/namespaces/Ember/methods/A?anchor=A', 'navigated to method');
 });
 
 test('Can navigate to property from namespace', async function(assert) {
   await visit('/ember/1.0/namespaces/Ember');
-  await click(`.spec-property-list ${testSelector('item', 'ENV')} a`);
+  await click(`.spec-property-list ${'[data-test-item="ENV"]'} a`);
 
   assert.equal(currentURL(), '/ember/1.0/namespaces/Ember/properties/ENV?anchor=ENV', 'navigated to property');
 });
 
 test('Can navigate to event from namespace', async function(assert) {
   await visit('/ember/1.0/namespaces/Ember');
-  await click(`.spec-event-list ${testSelector('item', 'onerror')} a`);
+  await click(`.spec-event-list ${'[data-test-item="onerror"]'} a`);
 
   assert.equal(currentURL(), '/ember/1.0/namespaces/Ember/events/onerror?anchor=onerror', 'navigated to event');
 });
