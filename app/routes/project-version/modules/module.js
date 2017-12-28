bbd2e0a696d862a0340f5158ebd77cfdbd915216
Todd Jordan <tjordan@tjordan-mbp.local>
2017-12-28 02:27:10 -0500

    refactor more code into main getFullVersion util

--- app/routes/project-version/modules/module.js
+++ app/routes/project-version/modules/module.js
@@ -1,7 +1,6 @@
 import ClassRoute from '../classes/class';
 import ScrollTracker from 'ember-api-docs/mixins/scroll-tracker';
-import getLastVersion from 'ember-api-docs/utils/get-last-version';
-import getCompactVersion from 'ember-api-docs/utils/get-compact-version';
+import getFullVersion from 'ember-api-docs/utils/get-full-version';
 
 export default ClassRoute.extend(ScrollTracker, {
 
@@ -9,14 +8,7 @@ export default ClassRoute.extend(ScrollTracker, {
     let projectID = transition.params['project-version'].project;
     let projectObj = await this.store.findRecord('project', projectID);
     let compactVersion = transition.params['project-version'].project_version;
-    let projectVersion;
-    if (compactVersion === 'release') {
-      let versions = projectObj.get('projectVersions').toArray();
-      projectVersion = getLastVersion(versions);
-      compactVersion = getCompactVersion(projectVersion);
-    } else {
-      projectVersion = this.get('metaStore').getFullVersion(projectID, compactVersion);
-    }
+    let projectVersion = getFullVersion(compactVersion, projectID, projectObj, this.get('metaStore'));
     let klass = params['module'];
     if (!~klass.indexOf(projectID) && klass !== 'rsvp' && klass !== 'jquery') {
       klass = `${projectID}-${klass}`;
--- app/routes/project-version/namespaces/namespace.js
+++ app/routes/project-version/namespaces/namespace.js
@@ -1,7 +1,6 @@
 import ClassRoute from '../classes/class';
 import ScrollTracker from 'ember-api-docs/mixins/scroll-tracker';
-import getLastVersion from 'ember-api-docs/utils/get-last-version';
-import getCompactVersion from 'ember-api-docs/utils/get-compact-version';
+import getFullVersion from 'ember-api-docs/utils/get-full-version';
 
 export default ClassRoute.extend(ScrollTracker, {
   templateName: 'project-version/classes/class',
@@ -10,14 +9,7 @@ export default ClassRoute.extend(ScrollTracker, {
     let projectID = transition.params['project-version'].project;
     let projectObj = await this.store.findRecord('project', projectID);
     let compactVersion = transition.params['project-version'].project_version;
-    let projectVersion;
-    if (compactVersion === 'release') {
-      let versions = projectObj.get('projectVersions').toArray();
-      projectVersion = getLastVersion(versions);
-      compactVersion = getCompactVersion(projectVersion);
-    } else {
-      projectVersion = this.get('metaStore').getFullVersion(projectID, compactVersion);
-    }
+    let projectVersion = getFullVersion(compactVersion, projectID, projectObj, this.get('metaStore'));
     const klass = params['namespace'];
     return this.find('namespace', `${projectID}-${projectVersion}-${klass}`);
   },
