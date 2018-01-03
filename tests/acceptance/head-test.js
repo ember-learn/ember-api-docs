5f3596a203986f4b310c87088a799b209a1099b6
Todd Jordan <tjordan@tjordan-mbp.local>
2018-01-03 02:22:06 -0500

    add link rel canonical for release url

---
+++ tests/acceptance/head-test.js
@@ -0,0 +1,31 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'ember-api-docs/tests/helpers/module-for-acceptance';
+import { visit } from 'ember-native-dom-helpers';
+import $ from 'jquery';
+
+moduleForAcceptance('Acceptance | head');
+
+test('shows link rel=canonical for release url', async function (assert) {
+  await visit('/ember/release/classes/Application');
+  assert.ok($('head link[rel=canonical]').attr('href'));
+});
+
+test('no link rel=canonical for version url', async function (assert) {
+  await visit('/ember/2.16/classes/Application');
+  assert.notOk($('head link[rel=canonical]').attr('href'));
+});
+
+test('no link rel=canonical when root url visited', async function (assert) {
+  await visit('/');
+  assert.notOk($('head link[rel=canonical]').attr('href'));
+})
+
+test('dns prefetch should be populated', async function (assert) {
+  await visit('/ember/release/classes/Application');
+  assert.equal($('head link[rel=dns-prefetch]').attr('href'), 'https://ember-api-docs.global.ssl.fastly.net');
+});
+
+test('dns prefetch should be populated when root url visited', async function (assert) {
+  await visit('/');
+  assert.equal($('head link[rel=dns-prefetch]').attr('href'), 'https://ember-api-docs.global.ssl.fastly.net');
+})
