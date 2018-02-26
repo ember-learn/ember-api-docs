4192f5fbd33dedb4c359310562df0cd0337a52de
Todd Jordan <todd.jordan@gmail.com>
2018-02-26 02:00:49 -0500

    Fix broken function links for rsvp and jquery

--- app/routes/project-version/functions/function.js
+++ app/routes/project-version/functions/function.js
@@ -18,12 +18,21 @@ export default Route.extend({
     let projectObj = await this.store.findRecord('project', projectID);
     let compactVersion = transition.params['project-version'].project_version;
     let projectVersion = getFullVersion(compactVersion, projectID, projectObj, this.get('metaStore'));
-    let className = params['module'];
+    let name = params['module'];
     let functionName = params['fn'];
-    let classObj = await this.store.find('class', `${projectID}-${projectVersion}-${className}`)
+    let fnModule;
+    let fn;
+    if (name === 'rsvp' || name === 'jquery') {
+      fnModule = await this.store.find('module', `${projectID}-${projectVersion}-${name}`);
+      fn = fnModule.get(`allstaticfunctions.${name}`).find(elem => elem.name === functionName);
+
+    } else {
+      fnModule = await this.store.find('class', `${projectID}-${projectVersion}-${name}`);
+      fn = this.getFunctionObjFromList(fnModule, functionName);
+    }
     return {
-      fnModule: classObj,
-      fn: this.getFunctionObjFromList(classObj, functionName)
+      fnModule,
+      fn
     };
   },
 
