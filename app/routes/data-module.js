c4bcbac0b7f37cc7ea1dc4e6ce1f5a6c650f6553
Sivakumar Kailasam <sivakumar-kailasam@users.noreply.github.com>
2018-05-07 19:34:32 +0530

    Revert "convert legacy urls to "release" route instead of 2.15"
    This reverts commit 6c54ebbe652f572e2bffad0dfe6af5f102df00ba.

--- app/routes/data-module.js
+++ app/routes/data-module.js
@@ -1,30 +1,48 @@
+import { hash, resolve } from 'rsvp';
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
+import { pluralize } from 'ember-inflector';
 
 export default Route.extend({
 
-  legacyModuleMappings: service(),
-
   model(params) {
-    return this.get('legacyModuleMappings').fetch()
-      .then((response) => response.json())
-      .then((mappings) => {
-        return {
-          moduleName: params.module.substr(0, params.module.lastIndexOf('.')),
-          mappings: this.get('legacyModuleMappings').buildMappings(mappings)
-        }
-      });
+    return this.get('store').findRecord('project', 'ember-data', { includes: 'project-version' })
+      .then((project) => {
+        let lastVersion = '2.15.3';
+        return this.get('store').findRecord('project-version', `ember-data-${lastVersion}`, { includes: 'project' });
+      })
+      .then((projectVersion) => {
+        let project = projectVersion.get('project');
+        let lastVersion = projectVersion.get('version');
+        let className = params['module'].substr(0, params['module'].lastIndexOf('.'));
+        let id = `ember-data-${lastVersion}-${className}`;
+        return hash({
+          project: resolve(project),
+          version: resolve(lastVersion),
+          classData: this.store.find('module', id)
+            .then((classData) => {
+              return {
+                type: 'module',
+                data: classData
+              };
+            })
+        }).catch(e => resolve({isError: true}));
+      }).catch(e => resolve({isError: true}));
   },
 
   redirect(model) {
-    let name = this.get('legacyModuleMappings').getNewModuleFromOld(model.moduleName, model.mappings);
-    if (name !== model.moduleName) {
-      return this.transitionTo(`project-version.modules.module`,
-        'ember-data',
-        'release',
-        name);
+    if (model.isError) {
+      return this.transitionTo('404');
     }
-    return this.transitionTo(`project-version`, 'ember-data', 'release');
+    return this.transitionTo(`project-version.${pluralize(model.classData.type)}.${model.classData.type}`,
+      model.project.id,
+      model.version,
+      model.classData.data.get('name'));
+  },
+
+  serialize(model) {
+    return {
+      namespace: model.classData.get('name')
+    }
   }
 
 });
