bda1643dedaad6f542a4511f4a0c1226c6c2ca01
Sivakumar Kailasam <siva@rubberduck.engineering>
2018-06-16 12:15:11 +0530

    Fix deprecations

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
 import resolver from './helpers/resolver';
-import { setResolver } from 'ember-qunit';
+import { setResolver } from '@ember/test-helpers';
 import loadEmberExam from 'ember-exam/test-support/load';
 import { start } from 'ember-cli-qunit';
 
