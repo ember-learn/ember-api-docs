51f0a1305691f369e1b87e82bdde0e9754b1cb0d
Oskar Rough <oskar@rough.dk>
2018-06-27 13:16:47 +0200

    Remove code examples from meta description

--- app/utils/create-excerpt.js
+++ app/utils/create-excerpt.js
@@ -1,14 +1,19 @@
 import sanitizeHtml from 'npm:sanitize-html'
 
 export default function createExcerpt(string, maxLength = 300) {
-  let excerpt = sanitizeHtml(string, {
-    // Don't allow any tags
-    allowedTags: [],
-    // Remove text inside these tags
-    nonTextTags: ['style', 'script', 'textarea', 'noscript', 'table']
-  }).replace(/\n/g, '')
+  // Stop at first code example
+  string = string.split('<table')[0]
+
+  let excerpt = sanitizeHtml(string, {allowedTags: []})
+    // Remove tabs and line breaks
+    .replace(/\t/g, '')
+    .replace(/\n/g, ' ')
+    .trim()
+
+  // Truncate
   if (excerpt.length > maxLength) {
-    excerpt = excerpt.slice(0, maxLength) + '…'
+    excerpt = excerpt.slice(0, maxLength - 1) + '…'
   }
+
   return excerpt
 }
