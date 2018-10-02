f7ef16c77fea3814ea3598ff8193e1f1f9f83bf1
Nick/Nelson <huggablesquare@gmail.com>
2018-10-02 00:00:10 -0400

    Remove test for open graph tag that was removed

--- tests/acceptance/open-graph-tags-test.js
+++ tests/acceptance/open-graph-tags-test.js
@@ -19,14 +19,3 @@ test('assigns title property', function (assert) {
   const title = findOpenGraphContent('title');
   assert.equal(title, 'Container - 1.0 - Ember API Documentation');
 });
-
-test('assigns image property and width/height', function (assert) {
-  const image = findOpenGraphContent('image');
-  assert.equal(image, 'assets/images/ember-logo.png');
-
-  const imageWidth = findOpenGraphContent('image:width');
-  assert.equal(imageWidth, '1200');
-
-  const imageHeight = findOpenGraphContent('image:height');
-  assert.equal(imageHeight, '1016');
-});
