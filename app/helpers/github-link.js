fed9511fc0428d7a163fed9b166104912f7f5a49
Todd Jordan <todd.jordan@gmail.com>
2018-10-25 01:42:11 -0400

    Showing pencil link only on current release, and linking to gh edit

--- app/helpers/github-link.js
+++ app/helpers/github-link.js
@@ -1,7 +1,10 @@
 import { helper } from '@ember/component/helper';
 import githubMap from '../utils/github-map';
 
-export function githubLink([project, version, file, line]) {
+export function githubLink([project, version, file, line], { isEdit=false }) {
+  if (isEdit) {
+    return `https://github.com/${githubMap[project]}/edit/release/${file}#L${line}`;
+  }
   return `https://github.com/${githubMap[project]}/tree/v${version}/${file}#L${line}`;
 }
 
