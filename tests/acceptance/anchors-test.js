efe6b5459c2ce666fa841954dab2e78ebe918e5d
Todd Jordan <todd.jordan@gmail.com>
2019-03-29 01:41:34 -0400

    Trim the inner text used to assert url value (#602)

--- tests/acceptance/anchors-test.js
+++ tests/acceptance/anchors-test.js
@@ -8,5 +8,5 @@ test('Can create a link from the "Properties" tab', async function(assert) {
   await visit('/ember/1.0/classes/Container/properties');
   let [ element ] = findAll('.class-field-description--link');
   await click(element);
-  assert.equal(currentURL(), `/ember/1.0/classes/Container/properties?anchor=${element.innerText}`);
+  assert.equal(currentURL(), `/ember/1.0/classes/Container/properties?anchor=${element.innerText.trim()}`);
 });
