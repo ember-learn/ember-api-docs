f885e7622fc81d2c469e334a5a6ea14c668449ed
Ilya Radchenko <knownasilya@gmail.com>
2019-03-29 18:14:38 -0400

    feat: sticky method, property, and event headers (#336)
    * feat: sticky method, property, and event headers
    
    * fix: h3 indentation for linting
    
    * fix template lint
    
    * update sticky dep
    
    * Only show sticky for areas with a greater height

--- app/components/class-field-description.js
+++ app/components/class-field-description.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
+import { gt } from '@ember/object/computed';
 
 export default Component.extend({
   legacyModuleMappings: service(),
@@ -8,6 +9,17 @@ export default Component.extend({
   hasImportExample: computed('field.{name,class}', function () {
     return this.get('legacyModuleMappings').hasFunctionMapping(this.get('field.name'), this.get('field.class'));
   }),
+
+  stickyEnabled: gt('ownHeight', 200),
+
+  didInsertElement() {
+    this._super(...arguments);
+    this.setOwnHeight(this.element);
+  },
+
+  setOwnHeight(element) {
+    this.set('ownHeight', element.clientHeight);
+  },
 
   /**
    * Callback for updating the anchor with the field name that was clicked by a user.
