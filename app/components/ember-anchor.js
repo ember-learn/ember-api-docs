f8964466e3aac00dc04f05dd8458eb6ba0f2baed
Djamel Bendaoud <djamel.bendaoud@qonto.eu>
2019-05-03 10:48:07 +0200

    ♻️ refact to extract offset logic in utils

--- app/components/ember-anchor.js
+++ app/components/ember-anchor.js
@@ -1,28 +1,19 @@
 import { get } from '@ember/object';
 import AnchorComponent from 'ember-anchor/components/ember-anchor';
 import config from 'ember-api-docs/config/environment';
+import getOffset from 'ember-api-docs/utils/get-offset';
 
 export default AnchorComponent.extend({
-  getOffset(element, container) {
-    let offsetTop = element.offsetTop;
-    let parent = element.offsetParent;
-    while (parent != null && parent != container) {
-      offsetTop  += parent.offsetTop;
-      parent = parent.offsetParent;
-    }
-    return offsetTop;
-  },
-
   // This overrides Ember Anchor to support scrolling within a fixed position element
   _scrollToElemPosition() {
     let qp = this.anchorQueryParam;
     let qpVal = this.get(get(this, 'attrs.a') ? 'a' : `controller.${qp}`);
     let elem = document.querySelector(`[data-${qp}="${qpVal}"]`);
-    let offset = elem.offsetHeight ? elem.offsetHeight : 0;
+    const offsetTop = elem.offsetHeight;
 
-    if (offset) {
-      const offsetTop = this.getOffset(elem, config.APP.scrollContainerSelector)
-      document.querySelector(config.APP.scrollContainerSelector).scrollTo(0, offsetTop);
+    if (offsetTop) {
+      const offsetToScroll = getOffset(elem, config.APP.scrollContainerSelector)
+      document.querySelector(config.APP.scrollContainerSelector).scrollTo(0, offsetToScroll);
     }
   }
 });
