ec3dcb8d0e625d504a2cfb7b7572991798da5c7c
Gaurav Munjal <Gaurav0@aol.com>
2019-09-24 23:11:03 -0400

    [BUGFIX] fix ember-data source links for v3.11+

--- app/utils/github-map.js
+++ app/utils/github-map.js
@@ -1,3 +1,17 @@
+import semverCompare from 'semver-compare';
+
+export const mainDir = function(project, version) {
+  if (project === 'ember') {
+    return '/';
+  }
+  if (project === 'ember-data') {
+    if (semverCompare(version, '3.11') === -1) {
+      return '/';
+    }
+    return '/packages/-ember-data/';
+  }
+};
+
 export default {
   'ember': 'emberjs/ember.js',
   'ember-data': 'emberjs/data'
