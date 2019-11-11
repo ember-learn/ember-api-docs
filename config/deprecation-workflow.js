cfbe2dd95e6a7a93d23ddfafc3f380137bf6cab7
Gaurav Munjal <Gaurav0@aol.com>
2019-11-10 20:07:49 -0500

    fix logger deprecation

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -4,6 +4,6 @@ self.deprecationWorkflow.config = {
     { handler: "throw", matchId: "ember-env.old-extend-prototypes" },
     { handler: "throw", matchId: "ember-routing.route-router" },
     { handler: "log", matchId: "ember-runtime.deprecate-copy-copyable" },
-    { handler: "log", matchId: "ember-console.deprecate-logger" }
+    { handler: "throw", matchId: "ember-console.deprecate-logger" }
   ]
 };
