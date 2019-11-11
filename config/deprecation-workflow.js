af0d3cc8ceca00b283b940a205a2759d88da7d0e
Gaurav Munjal <Gaurav0@aol.com>
2019-11-10 19:58:14 -0500

    Fix extend prototypes deprecation

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,7 +1,7 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
-    { handler: "log", matchId: "ember-env.old-extend-prototypes" },
+    { handler: "throw", matchId: "ember-env.old-extend-prototypes" },
     { handler: "throw", matchId: "ember-routing.route-router" },
     { handler: "log", matchId: "ember-runtime.deprecate-copy-copyable" },
     { handler: "log", matchId: "ember-console.deprecate-logger" }
