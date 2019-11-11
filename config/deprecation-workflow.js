0cdf7e4c3f4f7aadfdd43bf6c3d21e0b90b94b6e
Gaurav Munjal <Gaurav0@aol.com>
2019-11-10 20:19:15 -0500

    fix ember-copy / copyable deprecation

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -3,7 +3,7 @@ self.deprecationWorkflow.config = {
   workflow: [
     { handler: "throw", matchId: "ember-env.old-extend-prototypes" },
     { handler: "throw", matchId: "ember-routing.route-router" },
-    { handler: "log", matchId: "ember-runtime.deprecate-copy-copyable" },
+    { handler: "throw", matchId: "ember-runtime.deprecate-copy-copyable" },
     { handler: "throw", matchId: "ember-console.deprecate-logger" }
   ]
 };
