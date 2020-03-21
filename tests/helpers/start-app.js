14e9fd34c4e16a64bea03502263d464caec9b93a
Sivakumar Kailasam <siva@rubberduck.engineering>
2020-03-21 09:42:17 -0400

    Replace merge with assign

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,5 +1,5 @@
 import { run } from '@ember/runloop';
-import { merge } from '@ember/polyfills';
+import { assign } from '@ember/polyfills';
 import Application from '../../app';
 import config from '../../config/environment';
 import registerPowerSelectHelpers from 'ember-power-select/test-support/helpers';
@@ -8,9 +8,9 @@ import './percy/register-helpers';
 registerPowerSelectHelpers();
 
 export default function startApp(attrs) {
-  let attributes = merge({}, config.APP);
+  let attributes = assign({}, config.APP);
   attributes.autoboot = true;
-  attributes = merge(attributes, attrs); // use defaults, but you can override;
+  attributes = assign(attributes, attrs); // use defaults, but you can override;
 
   return run(() => {
     let application = Application.create(attributes);
