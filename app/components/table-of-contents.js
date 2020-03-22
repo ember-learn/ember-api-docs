11c98caf91a867ff2d78890d0093038b5f4061e0
Sivakumar Kailasam <siva@rubberduck.engineering>
2020-03-22 00:43:38 -0400

    Use default transition

--- app/components/table-of-contents.js
+++ app/components/table-of-contents.js
@@ -1,11 +1,8 @@
 import Component from '@glimmer/component';
-import fade from 'ember-animated/transitions/fade';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class TableOfContentsComponent extends Component {
-  transition = fade;
-
   @tracked showModules = true;
   @tracked showNamespaces = true;
   @tracked showClasses = true;
