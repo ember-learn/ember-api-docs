a80907cd52d89faf34b863b241709a64cb12851b
Sivakumar Kailasam <siva@rubberduck.engineering>
2020-03-22 00:17:48 -0400

    Migrate toc to glimmer component

--- app/components/table-of-contents.js
+++ app/components/table-of-contents.js
@@ -1,9 +1,17 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import fade from 'ember-animated/transitions/fade';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-export default Component.extend({
-  actions: {
-    toggle(type) {
-      this.$('ol.toc-level-1.' + type).slideToggle(200);
-    }
+export default class TableOfContentsComponent extends Component {
+  transition = fade;
+
+  @tracked showModules = true;
+  @tracked showNamespaces = true;
+  @tracked showClasses = true;
+
+  @action
+  toggle(prop) {
+    this[prop] = !this[prop];
   }
-});
+}
