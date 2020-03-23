bd279e7e830d0d50fee6d8eccf267062beb751ae
Gaurav Munjal <Gaurav0@aol.com>
2020-03-22 21:49:33 -0400

    silence deprecations to shorten log length

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,9 +1,8 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
-    { handler: "throw", matchId: "ember-env.old-extend-prototypes" },
-    { handler: "throw", matchId: "ember-routing.route-router" },
-    { handler: "throw", matchId: "ember-runtime.deprecate-copy-copyable" },
-    { handler: "throw", matchId: "ember-console.deprecate-logger" }
+    { handler: "silence", matchId: "transition-state" },
+    { handler: "silence", matchId: "deprecate-router-events" },
+    { handler: "silence", matchId: "ember-native-dom-helpers-click" }
   ]
 };
