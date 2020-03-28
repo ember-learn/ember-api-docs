2b893fd0907e206ffa714dc3ac3d5bc928b5b0ce
Gaurav Munjal <Gaurav0@aol.com>
2020-03-28 17:58:47 -0400

    fix async request leaks in fastboot

--- app/utils/get-full-version.js
+++ app/utils/get-full-version.js
@@ -4,7 +4,7 @@ import getLastVersion from 'ember-api-docs/utils/get-last-version';
 export default function getFullVersion(urlVersion, project, projectObj, metaStore) {
   let projectVersion;
   if (urlVersion === 'release') {
-    let versions = projectObj.get('projectVersions').toArray();
+    let versions = projectObj.hasMany('projectVersions').ids();
     projectVersion = metaStore.getFullVersion(project, getCompactVersion(getLastVersion(versions)));
   } else {
     projectVersion = metaStore.getFullVersion(project, urlVersion);
