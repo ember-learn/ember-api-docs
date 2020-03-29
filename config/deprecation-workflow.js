8c852efa8529dd1d098367d4b5fcd6346d127a21
Gaurav Munjal <Gaurav0@aol.com>
2020-03-29 09:32:20 -0400

    remove line from deprecation workflow

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,7 +2,6 @@ self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
     { handler: "silence", matchId: "transition-state" },
-    { handler: "throw", matchId: "deprecate-router-events" },
-    { handler: "throw", matchId: "ember-native-dom-helpers-click" }
+    { handler: "throw", matchId: "deprecate-router-events" }
   ]
 };
