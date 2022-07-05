900fcb7c417e9e3883a5a951b45d60ca8ec37ebc
Geordan Neukum <gneukum1@gmail.com>
2022-07-04 23:12:16 -0400

    import-example: update to glimmer
    Let's update the ImportExample component to extend glimmer/component
    instead of ember/component. Glimmer components offer a bunch of various
    advantages over classic components and are the direction that ember is
    heading in general.

--- app/components/import-example.js
+++ app/components/import-example.js
@@ -1,12 +1,14 @@
-/* eslint-disable ember/classic-decorator-no-classic-methods */
 import { action } from '@ember/object';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import { later } from '@ember/runloop';
+import { tracked } from '@glimmer/tracking';
 
 export default class ImportExample extends Component {
+  @tracked showClipboardSuccessIcon = false;
+
   @action
   showSuccess() {
-    this.toggleProperty('showClipboardSuccessIcon');
-    later(this, () => this.toggleProperty('showClipboardSuccessIcon'), 950);
+    this.showClipboardSuccessIcon = true;
+    later(this, () => (this.showClipboardSuccessIcon = false), 950);
   }
 }
