b9c198ae7434d2872332a71ff6213a581e9cce4e
Geordan Neukum <gneukum1@gmail.com>
2022-07-22 08:00:18 -0400

    dropdown-header: convert to glimmer
    Let's update the DropdownHeader component to extend glimmer/component
    instead of ember/component. Glimmer components offer a bunch of various
    advantages over classic components and are the direction that ember is
    heading in general.

--- app/components/search-input/dropdown-header.js
+++
@@ -1,5 +0,0 @@
-import { classNames } from '@ember-decorators/component';
-import Component from '@ember/component';
-
-@classNames('ds-suggestion')
-export default class DropdownHeader extends Component {}
