54ddf5385755e5fc9bd7393e6221827ebf6b7671
Chris Manson <chris@manson.ie>
2022-10-15 14:02:34 +0100

    add all routes for classes namespaces and modules

--- prember-urls.js
+++ prember-urls.js
@@ -1,4 +1,6 @@
 const { readdirSync } = require('fs');
+const cmp = require('semver-compare');
+const semver = require('semver');
 
 module.exports = function () {
   const projects = readdirSync('ember-api-docs-data/json-docs');
@@ -9,16 +11,43 @@ module.exports = function () {
     // add release for each of the projects
     urls.push(`/${p}/release`);
 
-    // add landing page for each of the projects versions
-    const projectVersions = readdirSync(`ember-api-docs-data/json-docs/${p}`)
-      .filter((v) => v.match(/\d+\.\d+\.\d+/))
-      .map((v) => {
-        let [, major, minor] = v.match(/(\d+)\.(\d+)\.\d+/);
-        return `${major}.${minor}`;
-      }); // uniq
+    const fullProjectVersions = readdirSync(
+      `ember-api-docs-data/json-docs/${p}`
+    ).filter((v) => v.match(/\d+\.\d+\.\d+/));
 
-    [...new Set(projectVersions)].forEach((v) => {
-      urls.push(`/${p}/${v}`);
+    // add landing page for each of the projects versions
+    const projectVersions = fullProjectVersions.map((v) => {
+      let [, major, minor] = v.match(/(\d+)\.(\d+)\.\d+/);
+      return `${major}.${minor}`;
+    }); // uniq
+
+    const uniqueProjectVersions = [...new Set(projectVersions)];
+
+    uniqueProjectVersions.forEach((uniqVersion) => {
+      urls.push(`/${p}/${uniqVersion}`);
+
+      const sortedPatchVersions = fullProjectVersions
+        .filter((projectVersion) => {
+          // console.log("comparing", projectVersion, uniqVersion, semver.satisfies(projectVersion, uniqVersion))
+          return semver.satisfies(projectVersion, uniqVersion);
+        })
+        .sort(cmp);
+
+      const highestPatchVersion =
+        sortedPatchVersions[sortedPatchVersions.length - 1];
+
+      const revIndex = require(`${__dirname}/ember-api-docs-data/rev-index/${p}-${highestPatchVersion}.json`);
+
+      ['classes', 'namespaces', 'modules'].forEach((entity) => {
+        // add classes
+        revIndex.data.relationships[entity].data.forEach(({id}) => {
+          const [, cleanId] = id.match(/^.+-\d+\.\d+\.\d+-(.*)/);
+          urls.push(`/${p}/${uniqVersion}/${entity}/${cleanId}`);
+          urls.push(`/${p}/${uniqVersion}/${entity}/${cleanId}/methods`);
+          urls.push(`/${p}/${uniqVersion}/${entity}/${cleanId}/properties`);
+          urls.push(`/${p}/${uniqVersion}/${entity}/${cleanId}/events`);
+        });
+      });
     });
   });
 
@@ -31,4 +60,6 @@ if (process.env.DEBUG === 'prember-urls') {
   let urls = module.exports();
 
   urls.forEach((url) => console.log(url));
+
+  console.log(`\n${urls.length} total URLs`);
 }
