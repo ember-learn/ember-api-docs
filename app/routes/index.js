c6f1f593e84d785f4da23e65fea32f29a3d5393f
Chris Krycho <hello@chriskrycho.com>
2022-11-18 17:31:02 -0700

    Switch away from deprecated Route.transitionTo

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,12 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
+  /** @type {import('@ember/routing/router-service').default} */
+  @service
+  router;
+
   redirect() {
-    return this.transitionTo('project', 'ember');
+    return this.router.transitionTo('project', 'ember');
   }
 }
