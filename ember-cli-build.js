2a7bdcb45a6f2a94c289647c9ea5daba153262d2
Anne-Greeth van Herwijnen <a.g.vanherwijnen@outlook.com>
2022-11-21 17:04:46 +0100

    remove all sass and ember-cli-sass

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,14 +20,6 @@ module.exports = function (defaults) {
       ],
       generateAssetMap: true,
     },
-    sassOptions: {
-      sourceMapEmbed: !envIsProduction,
-      includePaths: [
-        'app/styles',
-        'node_modules/bourbon-neat/app/assets/stylesheets',
-        'node_modules/normalize.css',
-      ],
-    },
     autoImport: {
       webpack: {
         node: {
