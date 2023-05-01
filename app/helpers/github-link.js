3d36c753859a8d9f27987fcd5518eae54d5f3709
Jen Weber <j@jenweber.me>
2023-05-01 15:30:37 -0400

    Update comments and use optional chaining

--- app/helpers/github-link.js
+++ app/helpers/github-link.js
@@ -1,12 +1,6 @@
 import { helper } from '@ember/component/helper';
 import githubMap, { mainDir } from '../utils/github-map';
 
-/**
- * https://github.com/emberjs/data/tree/v4.10.0/packages/packages/
- *   store/addon/-private/record-arrays/identifier-array.ts#L118
- *
- * File: ../packages/store/addon/-private/record-arrays/identifier-array.ts
- */
 export function githubLink([project, version, file, line], { isEdit = false }) {
   if (isEdit) {
     return `https://github.com/${githubMap[project]}/edit/release${mainDir(
@@ -19,7 +13,11 @@ export function githubLink([project, version, file, line], { isEdit = false }) {
   // add duplicate packages via the mainDir function.
   // Fixes an issue with ember data URLS having an incorrect
   // 'packages/packages' in the GitHub source URL
-  const fixedFile = file.replace('../packages/', '../');
+  // For example, without this fixedFile line, a `file` with value
+  // '../packages/store/addon/-private/record-arrays/identifier-array.ts'
+  // would become 
+  // 'https://github.com/emberjs/data/tree/v4.10.0/packages/packages/store/addon/-private/record-arrays/identifier-array.ts#L118'
+  const fixedFile = file?.replace('../packages/', '../');
 
   const result = `https://github.com/${
     githubMap[project]
