4185932c458d8376059c9a4771d9b4ae93fc69a5
Jen Weber <j@jenweber.me>
2023-05-08 11:28:04 -0400

    More code comments

--- app/helpers/github-link.js
+++ app/helpers/github-link.js
@@ -9,6 +9,9 @@ export function githubLink([project, version, file, line], { isEdit = false }) {
     )}${file}#L${line}`;
   }
 
+  // This 'packages' replacement can be removed if the following PR goes into a patch release of
+  // Ember Data 4.12: https://github.com/emberjs/data/pull/8598/files
+  //
   // If the file has packages already in the path, make sure we don't
   // add duplicate packages via the mainDir function.
   // Fixes an issue with ember data URLS having an incorrect
