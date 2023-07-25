26fbce7841667c391e56240fd4d533acbd97eba9
Andrew Pellerano <2031379+a13o@users.noreply.github.com>
2023-07-25 02:38:16 +0000

    Add test for stale results

--- tests/acceptance/search-test.js
+++ tests/acceptance/search-test.js
@@ -1,8 +1,10 @@
 import { module, test } from 'qunit';
-import { visit, currentURL, fillIn, click } from '@ember/test-helpers';
+import { visit, currentURL, fillIn, click, focus } from '@ember/test-helpers';
+import { selectChoose } from 'ember-power-select/test-support';
 import { setupApplicationTest } from 'ember-qunit';
 
-import searchResults from 'ember-api-docs/tests/fixtures/searchresult';
+import searchResultsV4_1 from 'ember-api-docs/tests/fixtures/searchresult-v-4-1';
+import searchResultsV5_1 from 'ember-api-docs/tests/fixtures/searchresult-v-5-1';
 
 module('Acceptance | search', function (hooks) {
   setupApplicationTest(hooks);
@@ -13,7 +15,7 @@ module('Acceptance | search', function (hooks) {
     const algoliaService = this.owner.lookup('service:algolia');
 
     algoliaService.search = async () => {
-      return searchResults;
+      return searchResultsV4_1;
     };
 
     await fillIn('[data-test-search-input]', 'forEach');
@@ -24,5 +26,33 @@ module('Acceptance | search', function (hooks) {
       currentURL(),
       '/ember/4.1/classes/EmberArray/methods/forEach?anchor=forEach'
     );
+  });
+
+  test('discard stale search results when version changes', async function (assert) {
+    await visit('/');
+
+    const algoliaService = this.owner.lookup('service:algolia');
+
+    algoliaService.search = async () => {
+      return searchResultsV4_1;
+    };
+
+    await selectChoose('.ember-power-select-trigger', '4.1');
+
+    await fillIn('[data-test-search-input]', 'get');
+
+    // the url contains /ember/4.1/
+    assert.dom('[data-test-search-result]').hasAttribute('href', /\/ember\/4\.1\//);
+
+    algoliaService.search = async () => {
+      return searchResultsV5_1;
+    };
+
+    await selectChoose('.ember-power-select-trigger', '5.1');
+
+    await focus('[data-test-search-input]');
+
+    // the url contains /ember/5.1/
+    assert.dom('[data-test-search-result]').hasAttribute('href', /\/ember\/5\.1\//);
   });
 });
