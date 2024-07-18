78c3d0d294f077f7efe8fc87bf3e0c48e624b2c5
Chris Manson <chris@manson.ie>
2024-07-18 11:01:22 +0100

    try vite

--- app/app.js
+++ app/app.js
@@ -1,7 +1,16 @@
 import Application from '@ember/application';
+import compatModules from '@embroider/core/entrypoint';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
-import config from 'ember-api-docs/config/environment';
+import config from './config/environment';
+
+let d = window.define;
+
+for (const [name, module] of Object.entries(compatModules)) {
+  d(name, function () {
+    return module;
+  });
+}
 
 import './deprecation-workflow';
 
---
+++ vite.config.mjs
@@ -0,0 +1,73 @@
+import { defineConfig } from 'vite';
+import {
+  resolver,
+  hbs,
+  scripts,
+  templateTag,
+  optimizeDeps,
+  compatPrebuild,
+  assets,
+  contentFor,
+} from '@embroider/vite';
+import { resolve } from 'path';
+import { babel } from '@rollup/plugin-babel';
+
+const root = 'tmp/rewritten-app';
+const extensions = [
+  '.mjs',
+  '.gjs',
+  '.js',
+  '.mts',
+  '.gts',
+  '.ts',
+  '.hbs',
+  '.json',
+];
+
+export default defineConfig(({ mode }) => {
+  return {
+    root,
+    cacheDir: resolve('node_modules', '.vite'),
+    resolve: {
+      extensions,
+    },
+    plugins: [
+      hbs(),
+      templateTag(),
+      scripts(),
+      resolver(),
+      compatPrebuild(),
+      assets(),
+      contentFor(),
+
+      babel({
+        babelHelpers: 'runtime',
+        extensions,
+      }),
+    ],
+    optimizeDeps: optimizeDeps(),
+    publicDir: resolve(process.cwd(), 'public'),
+    server: {
+      port: 4200,
+      watch: {
+        ignored: ['!**/tmp/rewritten-app/**'],
+      },
+    },
+    build: {
+      outDir: resolve(process.cwd(), 'dist'),
+      rollupOptions: {
+        input: {
+          main: resolve(root, 'index.html'),
+          ...(shouldBuildTests(mode)
+            ? { tests: resolve(root, 'tests/index.html') }
+            : undefined),
+        },
+      },
+    },
+  };
+});
+
+function shouldBuildTests(mode) {
+  return false;
+  return mode !== 'production' || process.env.FORCE_BUILD_TESTS;
+}
