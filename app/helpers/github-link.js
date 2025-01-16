62baf8e381a0088d33ddcb6f8b0d84100c0a2c1d
Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
2025-01-17 02:28:01 +0530

    fix: tests

--- app/helpers/github-link.js
+++ app/helpers/github-link.js
@@ -3,13 +3,15 @@ import githubMap, { mainDir } from '../utils/github-map';
 
 export function githubLink([project, version, file, line], { isEdit = false }) {
   const isEmberProject = project === 'ember';
-  const majorVersion = parseInt(version.split('.')[0].replace('v', ''), 10);
+  const majorVersion = parseInt(version?.split('.')[0].replace('v', ''), 10);
 
   const baseVersion = `v${version.replace(/^v/, '')}`;
 
   // Check if the project is 'ember' and adjust the tag only if the major version is >= 6 to match the Git tags
   const adjustedVersion =
-    isEmberProject && majorVersion >= 6 ? `${baseVersion}-ember-source` : baseVersion;
+    isEmberProject && majorVersion >= 6
+    ? `${baseVersion}-ember-source`
+     : baseVersion;
 
   if (isEdit) {
     return `https://github.com/${githubMap[project]}/edit/release${mainDir(
@@ -31,7 +33,9 @@ export function githubLink([project, version, file, line], { isEdit = false }) {
   // 'https://github.com/emberjs/data/tree/v4.10.0/packages/packages/store/addon/-private/record-arrays/identifier-array.ts#L118'
   const fixedFile = file?.replace('../packages/', '../');
 
-  return `https://github.com/${githubMap[project]}/tree/${adjustedVersion}${mainDir(
+  return `https://github.com/${
+    githubMap[project]
+  }/tree/${adjustedVersion}${mainDir(
     project,
     adjustedVersion
   )}${fixedFile}#L${line}`;
