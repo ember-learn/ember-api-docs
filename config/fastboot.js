6044001da938fafd5988495500fc6d58a74dfabb
Katie Gengler <katie@kmg.io>
2025-08-19 01:34:56 -0400

    Fix fetching data in fastboot/prember

--- config/fastboot.js
+++ config/fastboot.js
@@ -3,6 +3,21 @@ module.exports = function () {
     buildSandboxGlobals(defaultGlobals) {
       return Object.assign({}, defaultGlobals, {
         atob: atob,
+        AbortController,
+        fetch: fetch,
+        ReadableStream:
+          typeof ReadableStream !== 'undefined'
+            ? ReadableStream
+            : require('node:stream/web').ReadableStream,
+        WritableStream:
+          typeof WritableStream !== 'undefined'
+            ? WritableStream
+            : require('node:stream/web').WritableStream,
+        TransformStream:
+          typeof TransformStream !== 'undefined'
+            ? TransformStream
+            : require('node:stream/web').TransformStream,
+        Headers: typeof Headers !== 'undefined' ? Headers : undefined,
       });
     },
   };
