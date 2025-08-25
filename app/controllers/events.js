85f1b02d0c8c5f4b366847ab8f05911ffdc3b858
Katie Gengler <katie@kmg.io>
2025-08-25 19:19:12 -0400

    Remove vestiges of routes that were removed

--- app/controllers/events.js
+++
@@ -1,7 +0,0 @@
-import { inject as service } from '@ember/service';
-import Controller from '@ember/controller';
-
-export default class EventsController extends Controller {
-  @service
-  filterData;
-}
