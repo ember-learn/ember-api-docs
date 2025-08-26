9d74f33ac8ebb6d5af0724ac55ec5ae57a16a742
Katie Gengler <katie@kmg.io>
2025-08-26 00:40:16 -0400

    Force a full reload when a ?anchor ends up in the not found to get netlify redirects. This is an issue when links within the app still reference ?anchor (perhaps from markdown). Our alternatives are to return to ?anchor or to add support to redirecting to #anchors to the Ember Router.

--- app/routes/not-found.js
+++ app/routes/not-found.js
@@ -11,4 +11,23 @@ export default class NotFoundRoute extends Route {
 
     this.fastboot.response.statusCode = 404;
   }
+
+  redirect() {
+    if (typeof window === 'undefined' || !window.location) {
+      return;
+    }
+    const url = new URL(window.location.href);
+    const anchor = url.searchParams.get('anchor');
+    const redirected = url.searchParams.get('redirected');
+    if (anchor && !redirected) {
+      url.searchParams.set('redirected', '1');
+      // If we get here with an anchor query param, it means something in the
+      // api docs app is continuing to link to it (sometimes in the markdown)
+      // Force full reload to get netlify redirects
+      window.location.replace(url.toString());
+      // A more elegant solution would be to restore the various routes that
+      // handled ?anchor previously and redirect from there but that would
+      // necessitate adding support to the Ember Router to redirect to #anchors
+    }
+  }
 }
